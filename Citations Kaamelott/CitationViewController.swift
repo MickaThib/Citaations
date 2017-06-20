aee89d8dbb7a7576eb435d0403f0ad180dc53f02
MickaThib <mickael.thibouret@icloud.com>
2017-06-20 12:02:22 +0200

    + recherche filtrée
    Le tableView renvoie une valeur qui sert à filtrer les recherches parmi les citations

--- Citations Kaamelott/CitationViewController.swift
+++ Citations Kaamelott/CitationViewController.swift
@@ -9,6 +9,7 @@
 // A FAIRE :
 //           Améliorer l'aspect de l'interface
 //           En retour, afficher uniquement les citations du personnage choisi
+//           Prévoir le cas où aucune citation n'est disponible
 
 import UIKit
 
@@ -25,6 +26,10 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
     var citations:[Citation] = []
     // Liste des auteurs de citations existants
     var auteurs:[String] = []
+    // Données issues du fichier json
+    var json:[String:Any]?
+    // Array contenant les infos filtrées
+    var citationsFiltrees:[Citation] = []
     
     var derniereCitation:Int = 0
     var auteurChoisi:String = "Tous"
@@ -35,6 +40,8 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
         personnageSelectedLabel.isHidden = true
         
         parseJSON()
+        versionActuelle = versionJSON()
+        enregistrerCitations()
         
         // Configure écran accueil
         PersonnageLabel.text = "Citations de Kaamelott"
@@ -43,25 +50,64 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
     
     @IBAction func citationAleatoireAction(_ sender: UIButton) {
         
-        var nombreAleatoire = Int(arc4random_uniform(UInt32(citations.count)))
-        // print("Nombre aléatoire : \(nombreAleatoire)")
+        var citationSelected:Citation?
+        var nombreAleatoire:Int = 0
         
-        // Changer si la citation est la même que la précédente
-        while nombreAleatoire == derniereCitation {
+        // Si tous les auteurs sont sélectionnés
+        if auteurChoisi == "Tous" {
             
-            if nombreAleatoire == 0 {
-                nombreAleatoire += 1
-            } else {
-                nombreAleatoire -= 1
+            nombreAleatoire = Int(arc4random_uniform(UInt32(citations.count)))
+            // print("Nombre aléatoire : \(nombreAleatoire)")
+            
+            // Changer si la citation est la même que la précédente
+            while nombreAleatoire == derniereCitation {
+                
+                if nombreAleatoire == 0 {
+                    nombreAleatoire += 1
+                } else {
+                    nombreAleatoire -= 1
+                }
+                // print("Nombre changé : nouveau nombre \(nombreAleatoire)")
             }
-            // print("Nombre changé : nouveau nombre \(nombreAleatoire)")
+            
+            citationSelected = citations[nombreAleatoire]
+            
+        } else { // Si la recherche est filtrée
+            
+            // On vide l'array de citations filtrées
+            citationsFiltrees.removeAll()
+            
+            // On remplit le tableau citationsFiltrees avec les citations qui correspondent à la recherche
+            for quote in citations {
+                if quote.auteur == auteurChoisi {
+                    citationsFiltrees.append(quote)
+                }
+                // print("Nombre de citations filtrées : \(citationsFiltrees.count)")
+            }
+            
+            nombreAleatoire = Int(arc4random_uniform(UInt32(citationsFiltrees.count)))
+            
+            // S'il y a plus d'une citation, changer si la citation est la même que la précédente
+            if citationsFiltrees.count > 1 {
+                
+            while nombreAleatoire == derniereCitation {
+                
+                    if nombreAleatoire == 0 {
+                        nombreAleatoire += 1
+                    } else {
+                        nombreAleatoire -= 1
+                    }
+                    // print("Nombre changé : nouveau nombre \(nombreAleatoire)")
+                }
+            }
+            
+            citationSelected = citationsFiltrees[nombreAleatoire]
         }
         
-        let citationSelected = citations[nombreAleatoire]
-        
-        
-        PersonnageLabel.text = citationSelected.auteur
-        CitationTextView.text = citationSelected.texte
+        if citationSelected != nil {
+            PersonnageLabel.text = citationSelected?.auteur
+            CitationTextView.text = citationSelected?.texte
+        }
         
         derniereCitation = nombreAleatoire
         
@@ -76,25 +122,27 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
     
     func parseJSON(){
         
-        var json:[String:Any]?
-        
         do {
             json = try JSONSerialization.jsonObject(with: dataFromJson("citationsKaamelott"), options: .mutableContainers) as? [String:Any]
         } catch {
             print("Erreur lors de la serialization")
         }
+    }
+    
+    // Retourne la version du fichier json
+    func versionJSON() -> Double {
         
-        
-        // Numéro de version
         guard let docInfo = json?["docinfo"] as? [String:Any],
             let version = docInfo["version"] as? Double else {
                 print("Erreur lecture json : Version")
-                return
+                return 0.0
         }
+        return version
+    }
+    
+    // Place les citations + les auteurs dans un array
+    func enregistrerCitations() {
         
-        versionActuelle = version
-        
-        // Citations
         if let quotes = json?["citations"] as? [[String:Any]] {
             
             // Vider l'array
@@ -109,8 +157,6 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
                     let nouvelleCitation = Citation(id: id, auteur: auteur, texte: texte)
                     citations.append(nouvelleCitation)
                     
-                    //print("\(nouvelleCitation.auteur) a dit : \(nouvelleCitation.texte) (\(nouvelleCitation.id))")
-                    
                     // Liste d'auteurs
                     if auteurs.contains(nouvelleCitation.auteur) == false {
                         auteurs.append(nouvelleCitation.auteur)
@@ -122,7 +168,7 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
             }
             
             print("Nombre de citations chargées : \(citations.count)")
-            print("Liste des auteurs : \(auteurs)")
+            // print("Liste des auteurs : \(auteurs)")
             
         }
         
@@ -134,10 +180,21 @@ class CitationViewController: UIViewController, dataFromAuteursTableViewDelegate
         auteurChoisi = auteur
         
         if auteurChoisi == "Tous"{
+            // Cache le label du personnage choisi
             personnageSelectedLabel.isHidden = true
         } else {
+            // Affiche le label du personnage choisi + personnalise le nom
             personnageSelectedLabel.isHidden = false
             personnageSelectedLabel.text = "\(auteur) uniquement"
+            
+            // Affiche la première citation
+            for quote in citations {
+                if quote.auteur == auteurChoisi {
+                    PersonnageLabel.text = quote.auteur
+                    CitationTextView.text = quote.texte
+                    break
+                }
+            }
         }
     }
     
